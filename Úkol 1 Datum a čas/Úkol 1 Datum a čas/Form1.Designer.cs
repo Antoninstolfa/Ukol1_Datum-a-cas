45509e07286eac1581a77d293a88932866899fa0
Antoninstolfa <97514759+Antoninstolfa@users.noreply.github.com>
2022-10-13 21:36:17 +0200

    Add files via upload

---
+++ Úkol 1 Datum a čas/Úkol 1 Datum a čas/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Úkol_1_Datum_a_čas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.OdpovedLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(36, 21);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 177);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Alfons;Mucha;30.8. 1987\r\nJosef;Strejda;23.5. 2005\r\nMartin; Vejdyman;12.1. 2005\r\nL" +
+    "inux;Jenanic;11.6. 1523\r\nUždal;Nevim;13.10 2022\r\n";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(84, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Nejstarší?";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 254);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nejstarší osoba je";
+            // 
+            // OdpovedLabel
+            // 
+            this.OdpovedLabel.AutoSize = true;
+            this.OdpovedLabel.Location = new System.Drawing.Point(134, 254);
+            this.OdpovedLabel.Name = "OdpovedLabel";
+            this.OdpovedLabel.Size = new System.Drawing.Size(13, 13);
+            this.OdpovedLabel.TabIndex = 3;
+            this.OdpovedLabel.Text = "?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OdpovedLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label OdpovedLabel;
+    }
+}
+
---
+++ Úkol 1 Datum a čas/Úkol 1 Datum a čas/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Úkol_1_Datum_a_čas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime Nejstarsi = new DateTime();
+            string vstup = "";
+            Nejstarsi = DateTime.Today;
+            for (int i = 0; i < textBox1.Lines.Count(); i++)
+            {
+                vstup = textBox1.Lines[i];
+                if(vstup > Nejstarsi)
+                {
+                    Nejstarsi = vstup;
+                }
+            }
+            OdpovedLabel.Text = Nejstarsi.ToString();
+        }
+    }
+}
