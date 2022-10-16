f232e2f3261430beeaecec65094a0c1a0f2266e6
Antoninstolfa <stolfaa.04@spst.eu>
2022-10-16 21:10:56 +0200

    Dokončení

--- Úkol 1 Datum a čas/Úkol 1 Datum a čas/Form1.Designer.cs
+++ Úkol 1 Datum a čas/Úkol 1 Datum a čas/Form1.Designer.cs
@@ -32,21 +32,22 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.OdpovedLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(36, 21);
+            this.textBox1.Location = new System.Drawing.Point(47, 77);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(170, 177);
             this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "Alfons;Mucha;30.8. 1987\r\nJosef;Strejda;23.5. 2005\r\nMartin; Vejdyman;12.1. 2005\r\nL" +
-    "inux;Jenanic;11.6. 1523\r\nUždal;Nevim;13.10 2022\r\n";
+            this.textBox1.Text = "Alfons;Mucha;30/8/1987\r\nJosef;Strejda;23/5/2005\r\nMartin; Vejdyman;12/1/2005\r\nLinu" +
+    "x;Jenanic;11/6/1523\r\nUždal;Nevim;13/10/2022\r\n";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(84, 204);
+            this.button1.Location = new System.Drawing.Point(95, 260);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -57,26 +58,36 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(36, 254);
+            this.label1.Location = new System.Drawing.Point(47, 310);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.Size = new System.Drawing.Size(53, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Nejstarší osoba je";
+            this.label1.Text = "Výsledek:";
             // 
             // OdpovedLabel
             // 
             this.OdpovedLabel.AutoSize = true;
-            this.OdpovedLabel.Location = new System.Drawing.Point(134, 254);
+            this.OdpovedLabel.Location = new System.Drawing.Point(106, 310);
             this.OdpovedLabel.Name = "OdpovedLabel";
             this.OdpovedLabel.Size = new System.Drawing.Size(13, 13);
             this.OdpovedLabel.TabIndex = 3;
             this.OdpovedLabel.Text = "?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(264, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Zadejte: jméno;příjmení;datum narození (10/15/2022)";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.OdpovedLabel);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
@@ -94,6 +105,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label OdpovedLabel;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Úkol 1 Datum a čas/Úkol 1 Datum a čas/Form1.cs
+++ Úkol 1 Datum a čas/Úkol 1 Datum a čas/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -20,17 +21,45 @@ namespace Úkol_1_Datum_a_čas
         private void button1_Click(object sender, EventArgs e)
         {
             DateTime Nejstarsi = new DateTime();
-            string vstup = "";
+            string JenomDen = Nejstarsi.ToString("d");
+            string pomocna = "";
+            string pomocna2 = JenomDen;
             Nejstarsi = DateTime.Today;
             for (int i = 0; i < textBox1.Lines.Count(); i++)
             {
-                vstup = textBox1.Lines[i];
-                if(vstup > Nejstarsi)
+                pomocna = "";
+                string text = textBox1.Lines[i];
+                foreach (char c in text)
                 {
-                    Nejstarsi = vstup;
+                    if (c >= '0' && c < '9' || c == '/')
+                    {
+                        pomocna += c;
+                    }
                 }
+                try
+                {
+                    if (Convert.ToDateTime(pomocna) < Convert.ToDateTime(JenomDen))
+                    {
+
+                        if (Convert.ToDateTime(pomocna2) > Convert.ToDateTime(pomocna))
+                        {
+                            pomocna2 = pomocna;
+                            OdpovedLabel.Text = "Nejstarší člověk je: " + text;
+                        }
+                        else if (Convert.ToDateTime(pomocna) == Convert.ToDateTime(pomocna2))
+                        {
+                            OdpovedLabel.Text += "\na stejně starý je i: " + text;
+                        }
+                    }
+                    else OdpovedLabel.Text = "Žádný člověk nenalezen";
+                }
+                catch
+                {
+                    MessageBox.Show("Pravděpodobně jsi zadal špatný formát data, závorky u nadpisu ti pomohou :D", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+                }
+                OdpovedLabel.Text = Nejstarsi.ToString();
             }
-            OdpovedLabel.Text = Nejstarsi.ToString();
         }
     }
 }
